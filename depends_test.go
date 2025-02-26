4db33d1bdf12c3a29cd2d7d2cd538a1b65ecadbb
Eliseo Martelli <me@eliseomartelli.it>
2025-02-26 13:48:48 +0100

    feat: add tests and dependency file for RcppArmadillo
    This commit adds a new test function `Test_Armadillo` to verify the
    functionality of a hypothetical `DependsOn` function with the
    RcppArmadillo package. Additionally, it includes a binary file
    `RcppArmadillo_14.2.3-1.tar.gz` in the `test_assets` directory, which is
    referenced by the new test.
    
    Changelog:
    - Added `Test_Armadillo` to `depends_test.go`
    - Included `RcppArmadillo_14.2.3-1.tar.gz` in `test_assets`

--- depends_test.go
+++ depends_test.go
@@ -23,6 +23,23 @@ func Test_Depends(t *testing.T) {
 	}
 }
 
+func Test_Armadillo(t *testing.T) {
+	pathToDependency := "./test_assets/RcppArmadillo_14.2.3-1.tar.gz"
+	dir, err := os.Getwd()
+	if err != nil {
+		t.Error(err)
+	}
+	newPath := path.Join(dir, pathToDependency)
+	dep, err := DependsOn(newPath)
+	if err != nil {
+		t.Error(err)
+	}
+	t.Log(dep)
+	if len(dep) < 1 {
+		t.Error("Expected at least one dependency.")
+	}
+}
+
 func Test_SystemRequirements(t *testing.T) {
 	pathToDependency := "./test_assets/RCurl_1.98-1.14.tar.gz"
 	dir, err := os.Getwd()
